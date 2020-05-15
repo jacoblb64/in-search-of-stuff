0fa530dab50686c65f37ff6c4d662498f223c189
Jacob Barnett <jacob@havenlife.com>
2020-05-15 15:32:45 -0400

    adding preset env next to postcss, allowing nesting of styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+const postcssPresetEnv = require("postcss-preset-env");
+
 module.exports = {
     plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
-    ]
-  }
+        postcssPresetEnv({
+            stage: 3,
+            features: {
+                "nesting-rules": true,
+            },
+        }),
+        require("tailwindcss"),
+        require("autoprefixer"),
+    ],
+};
